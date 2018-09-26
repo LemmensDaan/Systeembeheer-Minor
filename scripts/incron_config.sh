6e3dc1518fcac71d2b7326750c1018491c4c5f1c
Daan Lemmens <daan.lemmens@uclllabs.be>
2018-09-26 15:27:09 +0000

    Script incron_config.sh now correct

--- scripts/incron_config.sh
+++ scripts/incron_config.sh
@@ -2,6 +2,5 @@
 while true
 do
 find /etc -type d ! -regex '.*/.git.*' -print0 | xargs -0 -I{} echo "{} IN_CLOSE_WRITE /etc/scripts/backup.sh \$@/\$#" > /etc/incron.d/etc.conf
-sleep 900 
-
+sleep 900
 done
