d745643e0e131fb56d1bc0d9f0cb7c882601b2c7
Daan Lemmens <daan.lemmens@uclllabs.be>
2018-10-03 12:17:25 +0000

    changed incron_config.sh and let the script run in cron.d dir

--- scripts/incron_config.sh
+++ scripts/incron_config.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-while true
-do
 find /etc -type d ! -regex '.*/.git.*' -print0 | xargs -0 -I{} echo "{} IN_CLOSE_WRITE /etc/scripts/backup.sh \$@/\$#" > /etc/incron.d/etc.conf
-sleep 900
-done
