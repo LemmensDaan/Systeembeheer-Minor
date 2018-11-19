8ab6d51059e109463d39067541424cc3886c45fb
Daan Lemmens <daan.lemmens@uclllabs.be>
2018-11-19 15:33:43 +0100

    commented bind-address of mariadb

---
+++ scripts/db_script.sql
@@ -0,0 +1,17 @@
+DELIMITER //
+
+USE check
+CREATE TRIGGER trigger1
+BEFORE INSERT 
+ON log FOR EACH ROW
+
+	BEGIN
+		SELECT COUNT(*) INTO @nr_rows FROM log;
+		IF @nr_rows >= 100 THEN
+			SIGNAL SQLSTATE '45000'
+				SET MESSAGE_TEXT = 'Cannot add row, max number of rows.';
+		END IF;
+	END; //
+
+DELIMITER ;
+
